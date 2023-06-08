6786c8eb9e9f52c978731b06cf7b90cfbcc8236a
OtakeYuto <yuto.programming@gmail.com>
2023-06-09 02:47:40 +0900

    htmlのtitleタグを削除

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def select():
-    users = db_session.query(User).all()
-    # users = db_session.query(User).first()
+    users = session.query(User).all()
+    # users = session.query(User).first()
     return render_template('index.html', users=users)
 
 # データ挿入
@@ -18,31 +18,31 @@ def insert():
     age = request.form.get('age')
     
     user = User(name, age)
-    db_session.add(user)
-    db_session.commit()
+    session.add(user)
+    session.commit()
     return redirect('/')
 
 # データ削除
 @app.route('/delete/<id>', methods=['POST'])
 def delete(id):
-    user = db_session.query(User).get(id)
-    db_session.delete(user)
-    db_session.commit()
+    user = session.query(User).get(id)
+    session.delete(user)
+    session.commit()
     return redirect('/')
     
 # データ編集
-@app.route('/edit/<id>', methods=['POST'])
+@app.route('/edit/<id>')
 def edit(id):
-    user = db_session.query(User).get(id)
+    user = session.query(User).get(id)
     return render_template('edit.html', user=user)
+
 # データ更新
 @app.route('/update/<id>', methods={'POST'})
 def update(id):
-    user = db_session.query(User).get(id)
+    user = session.query(User).get(id)
     user.name = request.form.get('name')
     user.age = request.form.get('age')
-    
-    db_session.commit()
+    session.commit()
     return redirect('/')
     
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 engine = create_engine('sqlite:///example.db', echo=True)
-db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False,bind=engine))
+session = scoped_session(sessionmaker(autocommit=False, autoflush=False,bind=engine))
 Base = declarative_base()
-Base.query = db_session.query_property()
+Base.query = session.query_property()
 
 if __name__ == '__main__':
     from models import *
