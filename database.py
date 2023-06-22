dff41e924f953589efc401acb8d8662c112dfb8f
OtakeYuto <yuto.programming@gmail.com>
2023-06-23 00:02:25 +0900

    remove comment

--- database.py
+++ database.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 engine = create_engine('sqlite:///sample.db')
 session = scoped_session(sessionmaker(autocommit=False, autoflush=False,bind=engine))
 Base = declarative_base()
-Base.query = session.query_property()
 
 if __name__ == '__main__':
     from models import *
